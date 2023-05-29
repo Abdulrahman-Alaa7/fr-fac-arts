592b32dbc4816bcf1d3e48110ac731ca638b3fde
Abdulrahman-Alaa7 <wl3elkobs@gmail.com>
2023-05-29 02:04:38 +0200

    update

--- js/main.js
+++ js/main.js
@@ -105,3 +105,26 @@ dark.addEventListener("click", function darkMode() {
     ev.classList.toggle("dark");
   });
 });
+
+// dropdown
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+const btn = document.querySelector(".dropbtn");
+
+btn.addEventListener("click", function () {
+  document.getElementById("myDropdown").classList.toggle("show");
+});
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
