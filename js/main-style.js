35fb8f6b8a83bce5c812500c56374fb796600f50
Abdulrahman-Alaa7 <wl3elkobs@gmail.com>
2023-06-14 23:13:08 +0200

    update

--- js/main-style.js
+++ js/main-style.js
@@ -20,214 +20,214 @@ tabsArray.forEach((ele) => {
   });
 });
 
-// One
+// // One
 
-const spanOne = document.querySelector(".span-one");
+// const spanOne = document.querySelector(".span-one");
 
-const audioOne = document.querySelector(".audio-one");
+// const audioOne = document.querySelector(".audio-one");
 
-const stopAudioOne = document.querySelector(".span-stop-one");
+// const stopAudioOne = document.querySelector(".span-stop-one");
 
-spanOne.addEventListener("click", function () {
-  audioOne.play();
-});
-stopAudioOne.addEventListener("click", function () {
-  audioOne.load();
-});
+// spanOne.addEventListener("click", function () {
+//   audioOne.play();
+// });
+// stopAudioOne.addEventListener("click", function () {
+//   audioOne.load();
+// });
 
-// Two
+// // Two
 
-const spanTwo = document.querySelector(".span-two");
+// const spanTwo = document.querySelector(".span-two");
 
-const audioTwo = document.querySelector(".audio-two");
+// const audioTwo = document.querySelector(".audio-two");
 
-const stopAudioTwo = document.querySelector(".span-stop-two");
+// const stopAudioTwo = document.querySelector(".span-stop-two");
 
-spanTwo.addEventListener("click", function () {
-  audioTwo.play();
-});
+// spanTwo.addEventListener("click", function () {
+//   audioTwo.play();
+// });
 
-stopAudioTwo.addEventListener("click", function () {
-  audioTwo.load();
-});
+// stopAudioTwo.addEventListener("click", function () {
+//   audioTwo.load();
+// });
 
-//Three
+// //Three
 
-const spanThree = document.querySelector(".span-three");
+// const spanThree = document.querySelector(".span-three");
 
-const audioThree = document.querySelector(".audio-three");
+// const audioThree = document.querySelector(".audio-three");
 
-const stopAudioThree = document.querySelector(".span-stop-three");
+// const stopAudioThree = document.querySelector(".span-stop-three");
 
-spanThree.addEventListener("click", function () {
-  audioThree.play();
-});
+// spanThree.addEventListener("click", function () {
+//   audioThree.play();
+// });
 
-stopAudioThree.addEventListener("click", function () {
-  audioThree.load();
-});
+// stopAudioThree.addEventListener("click", function () {
+//   audioThree.load();
+// });
 
-// Four
+// // Four
 
-const spanFour = document.querySelector(".span-four");
+// const spanFour = document.querySelector(".span-four");
 
-const audioFour = document.querySelector(".audio-four");
+// const audioFour = document.querySelector(".audio-four");
 
-const stopAudioFour = document.querySelector(".span-stop-four");
+// const stopAudioFour = document.querySelector(".span-stop-four");
 
-spanFour.addEventListener("click", function () {
-  audioFour.play();
-});
+// spanFour.addEventListener("click", function () {
+//   audioFour.play();
+// });
 
-stopAudioFour.addEventListener("click", function () {
-  audioFour.load();
-});
+// stopAudioFour.addEventListener("click", function () {
+//   audioFour.load();
+// });
 
-// Five
-const spanFive = document.querySelector(".span-five");
+// // Five
+// const spanFive = document.querySelector(".span-five");
 
-const audioFive = document.querySelector(".audio-five");
+// const audioFive = document.querySelector(".audio-five");
 
-const stopAudioFive = document.querySelector(".span-stop-five");
+// const stopAudioFive = document.querySelector(".span-stop-five");
 
-spanFive.addEventListener("click", function () {
-  audioFive.play();
-});
+// spanFive.addEventListener("click", function () {
+//   audioFive.play();
+// });
 
-stopAudioFive.addEventListener("click", function () {
-  audioFive.load();
-});
-//Six
-const spanSix = document.querySelector(".span-six");
+// stopAudioFive.addEventListener("click", function () {
+//   audioFive.load();
+// });
+// //Six
+// const spanSix = document.querySelector(".span-six");
 
-const audioSix = document.querySelector(".audio-six");
+// const audioSix = document.querySelector(".audio-six");
 
-const stopAudioSix = document.querySelector(".span-stop-six");
+// const stopAudioSix = document.querySelector(".span-stop-six");
 
-spanSix.addEventListener("click", function () {
-  audioSix.play();
-});
+// spanSix.addEventListener("click", function () {
+//   audioSix.play();
+// });
 
-stopAudioSix.addEventListener("click", function () {
-  audioSix.load();
-});
-//Seven
-const spanSeven = document.querySelector(".span-seven");
+// stopAudioSix.addEventListener("click", function () {
+//   audioSix.load();
+// });
+// //Seven
+// const spanSeven = document.querySelector(".span-seven");
 
-const audioSeven = document.querySelector(".audio-seven");
+// const audioSeven = document.querySelector(".audio-seven");
 
-const stopAudioSeven = document.querySelector(".span-stop-seven");
+// const stopAudioSeven = document.querySelector(".span-stop-seven");
 
-spanSeven.addEventListener("click", function () {
-  audioSeven.play();
-});
+// spanSeven.addEventListener("click", function () {
+//   audioSeven.play();
+// });
 
-stopAudioSeven.addEventListener("click", function () {
-  audioSeven.load();
-});
-//Eight
-const spanEight = document.querySelector(".span-eight");
+// stopAudioSeven.addEventListener("click", function () {
+//   audioSeven.load();
+// });
+// //Eight
+// const spanEight = document.querySelector(".span-eight");
 
-const audioEight = document.querySelector(".audio-eight");
+// const audioEight = document.querySelector(".audio-eight");
 
-const stopAudioEight = document.querySelector(".span-stop-eight");
+// const stopAudioEight = document.querySelector(".span-stop-eight");
 
-spanEight.addEventListener("click", function () {
-  audioEight.play();
-});
+// spanEight.addEventListener("click", function () {
+//   audioEight.play();
+// });
 
-stopAudioEight.addEventListener("click", function () {
-  audioEight.load();
-});
+// stopAudioEight.addEventListener("click", function () {
+//   audioEight.load();
+// });
 
-//Nine
-const spanNine = document.querySelector(".span-nine");
+// //Nine
+// const spanNine = document.querySelector(".span-nine");
 
-const audioNine = document.querySelector(".audio-nine");
+// const audioNine = document.querySelector(".audio-nine");
 
-const stopAudioNine = document.querySelector(".span-stop-nine");
+// const stopAudioNine = document.querySelector(".span-stop-nine");
 
-spanNine.addEventListener("click", function () {
-  audioNine.play();
-});
+// spanNine.addEventListener("click", function () {
+//   audioNine.play();
+// });
 
-stopAudioNine.addEventListener("click", function () {
-  audioNine.load();
-});
-//Ten
-const spanTen = document.querySelector(".span-ten");
+// stopAudioNine.addEventListener("click", function () {
+//   audioNine.load();
+// });
+// //Ten
+// const spanTen = document.querySelector(".span-ten");
 
-const audioTen = document.querySelector(".audio-ten");
+// const audioTen = document.querySelector(".audio-ten");
 
-const stopAudioTen = document.querySelector(".span-stop-ten");
+// const stopAudioTen = document.querySelector(".span-stop-ten");
 
-spanTen.addEventListener("click", function () {
-  audioTen.play();
-});
+// spanTen.addEventListener("click", function () {
+//   audioTen.play();
+// });
 
-stopAudioTen.addEventListener("click", function () {
-  audioTen.load();
-});
+// stopAudioTen.addEventListener("click", function () {
+//   audioTen.load();
+// });
 
-//Eleven
-const spanEleven = document.querySelector(".span-eleven");
+// //Eleven
+// const spanEleven = document.querySelector(".span-eleven");
 
-const audioEleven = document.querySelector(".audio-eleven");
+// const audioEleven = document.querySelector(".audio-eleven");
 
-const stopAudioEleven = document.querySelector(".span-stop-eleven");
+// const stopAudioEleven = document.querySelector(".span-stop-eleven");
 
-spanEleven.addEventListener("click", function () {
-  audioEleven.play();
-});
+// spanEleven.addEventListener("click", function () {
+//   audioEleven.play();
+// });
 
-stopAudioEleven.addEventListener("click", function () {
-  audioEleven.load();
-});
+// stopAudioEleven.addEventListener("click", function () {
+//   audioEleven.load();
+// });
 
-//Twelve
-const spanTwelve = document.querySelector(".span-twelve");
+// //Twelve
+// const spanTwelve = document.querySelector(".span-twelve");
 
-const audioTwelve = document.querySelector(".audio-twelve");
+// const audioTwelve = document.querySelector(".audio-twelve");
 
-const stopAudioTwelve = document.querySelector(".span-stop-twelve");
+// const stopAudioTwelve = document.querySelector(".span-stop-twelve");
 
-spanTwelve.addEventListener("click", function () {
-  audioTwelve.play();
-});
+// spanTwelve.addEventListener("click", function () {
+//   audioTwelve.play();
+// });
 
-stopAudioTwelve.addEventListener("click", function () {
-  audioTwelve.load();
-});
+// stopAudioTwelve.addEventListener("click", function () {
+//   audioTwelve.load();
+// });
 
-//Thirteen
-const spanThirteen = document.querySelector(".span-thirteen");
+// //Thirteen
+// const spanThirteen = document.querySelector(".span-thirteen");
 
-const audioThirteen = document.querySelector(".audio-thirteen");
+// const audioThirteen = document.querySelector(".audio-thirteen");
 
-const stopAudioThirteen = document.querySelector(".span-stop-thirteen");
+// const stopAudioThirteen = document.querySelector(".span-stop-thirteen");
 
-spanThirteen.addEventListener("click", function () {
-  audioThirteen.play();
-});
+// spanThirteen.addEventListener("click", function () {
+//   audioThirteen.play();
+// });
 
-stopAudioThirteen.addEventListener("click", function () {
-  audioThirteen.load();
-});
+// stopAudioThirteen.addEventListener("click", function () {
+//   audioThirteen.load();
+// });
 
-//Fourteen
-const spanFourteen = document.querySelector(".span-fourteen");
+// //Fourteen
+// const spanFourteen = document.querySelector(".span-fourteen");
 
-const audioFourteen = document.querySelector(".audio-fourteen");
+// const audioFourteen = document.querySelector(".audio-fourteen");
 
-const stopAudioFourteen = document.querySelector(".span-stop-fourteen");
+// const stopAudioFourteen = document.querySelector(".span-stop-fourteen");
 
-spanFourteen.addEventListener("click", function () {
-  audioFourteen.play();
-});
+// spanFourteen.addEventListener("click", function () {
+//   audioFourteen.play();
+// });
 
-stopAudioFourteen.addEventListener("click", function () {
-  audioFourteen.load();
-});
+// stopAudioFourteen.addEventListener("click", function () {
+//   audioFourteen.load();
+// });
 
 // top button
 
