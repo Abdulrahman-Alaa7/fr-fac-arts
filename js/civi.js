512379f9d23889f47217afac357c66e47b3a1b1a
Abdulrahman-Alaa7 <wl3elkobs@gmail.com>
2023-06-19 23:30:08 +0200

    update

--- js/civi.js
+++ js/civi.js
@@ -78,11 +78,11 @@ var x = setInterval(function () {
 
   // If the count down is finished, write some text
   if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("days").innerHTML = "0";
-    document.getElementById("hours").innerHTML = "0";
-    document.getElementById("min").innerHTML = "0";
-    document.getElementById("sec").innerHTML = "0";
+    // clearInterval(x);
+    // document.getElementById("days").innerHTML = "0";
+    // document.getElementById("hours").innerHTML = "0";
+    // document.getElementById("min").innerHTML = "0";
+    // document.getElementById("sec").innerHTML = "0";
   }
 }, 1000);
 
